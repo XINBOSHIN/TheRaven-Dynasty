479268ccd99bfcba05d762a7e5afe42abd6c9bd6
Mortobello <danilnovichek@vk.com>
2022-06-17 03:02:30 +0300

    Удаление главного файла RavenDynasty.java для избежания дальнейших проблем компиляции

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+    public static final String NAME = "Raven Dynasty";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
