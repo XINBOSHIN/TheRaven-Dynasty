2d9a6c7459cdff758ca31d059a2f2ef74c9b1832
Xinboshin Butterworth <danilnovichek@vk.com>
2023-06-12 23:47:29 +0500

    Update ItemsRegistry.java
    Co-Authored-By: Forohon <90405389+krutoytimati@users.noreply.github.com>
    Co-Authored-By: nik1000-7 <116767470+nik1000-7@users.noreply.github.com>
    Co-Authored-By: Deevate <125690574+Deevate@users.noreply.github.com>

--- src/main/java/com/xinboshin/theravendynasty/item/ItemsRegistry.java
+++ src/main/java/com/xinboshin/theravendynasty/item/ItemsRegistry.java
@@ -4,29 +4,16 @@ package com.xinboshin.theravendynasty.item;
 @ObjectHolder("theravendynasty")
 @Mod.EventBusSubscriber// Автоматическая регистрация статичных обработчиков событий
 public class ItemsRegistry {
-    /*
-     * Получение предмета по ключу. Вы также можете использовать данную аннотацию для получения ванильных предметов
-     * Если вы не добавляли аннотация над классом, то в таком случаи вам нужно прописать вместо `key` -> `tut:key`
-     */
     @ObjectHolder("key")
     public static final Item KEY = null;
 
-    /*
-     * Начиная с 1.12 регистрацию предметов/блоков/моделей и т.п. следует проводить в специальном событии.
-     * Событие Register<IForgeRegistryEntry> поддерживает регистрацию: Block, Item, Potion, Biome, SoundEvent,
-     * PotionType, Enchantment, IRecipe,  VillagerProfession, EntityEntry.
-     * Обратите внимание! Метод является статичным, так как мы используем EventBusSubscriber
-     */
+
     @SubscribeEvent
     public static void onRegistryItem(RegistryEvent.Register<Item> e) {
         // Также вместо `register` можно использовать `registerAll`, чтобы прописать все предметы разом
         e.getRegistry().register(new ItemKey());
     }
-
-    /*
-     * Начиная с 1.11 регистрацию моделей для предметов/блоков следует проводить в специальном событии.
-     * Обратите внимание! Метод является статичным, так как мы используем EventBusSubscriber
-     */
+    
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public static void onRegistryModel(ModelRegistryEvent e) {
