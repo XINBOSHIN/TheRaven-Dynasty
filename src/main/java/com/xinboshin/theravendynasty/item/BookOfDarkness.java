ae17c6d2ea603e0ea8b41d924c5b4e2c49e7e302
Xinboshin Butterworth <danilnovichek@vk.com>
2023-06-12 23:51:57 +0500

    Update BookOfDarkness.java
    Co-Authored-By: Forohon <90405389+krutoytimati@users.noreply.github.com>
    Co-Authored-By: nik1000-7 <116767470+nik1000-7@users.noreply.github.com>
    Co-Authored-By: Deevate <125690574+Deevate@users.noreply.github.com>

--- src/main/java/com/xinboshin/theravendynasty/item/BookOfDarkness.java
+++ src/main/java/com/xinboshin/theravendynasty/item/BookOfDarkness.java
@@ -3,7 +3,7 @@ package com.xinboshin.theravendynasty.item;
 import net.minecraft.item.Item;
 
 public class BookOfDarkness extends Item {
-    public BookOfDarkness() {
+    public BookOfDarkness() { //неизвестная проблема и аннотация ObjectHolder
         this.setRegistryName("bookofthedarkness");
         this.setUnlocalizedName("bookofthedarkness");
     }
