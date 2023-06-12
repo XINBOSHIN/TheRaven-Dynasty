ef228f34b198c78bf5794b747415828b230f4034
Xinboshin Butterworth <danilnovichek@vk.com>
2023-06-12 23:48:18 +0500

    Update ItemsRegistry.java
    Co-Authored-By: Forohon <90405389+krutoytimati@users.noreply.github.com>
    Co-Authored-By: nik1000-7 <116767470+nik1000-7@users.noreply.github.com>
    Co-Authored-By: Deevate <125690574+Deevate@users.noreply.github.com>

--- src/main/java/com/xinboshin/theravendynasty/item/ItemsRegistry.java
+++ src/main/java/com/xinboshin/theravendynasty/item/ItemsRegistry.java
@@ -4,7 +4,7 @@ package com.xinboshin.theravendynasty.item;
 @ObjectHolder("theravendynasty")
 @Mod.EventBusSubscriber// Автоматическая регистрация статичных обработчиков событий
 public class ItemsRegistry {
-    @ObjectHolder("key")
+    @ObjectHolder("bookofdarkness")
     public static final Item KEY = null;
 
 
@@ -13,7 +13,7 @@ public class ItemsRegistry {
         // Также вместо `register` можно использовать `registerAll`, чтобы прописать все предметы разом
         e.getRegistry().register(new ItemKey());
     }
-    
+
     @SubscribeEvent
     @SideOnly(Side.CLIENT)
     public static void onRegistryModel(ModelRegistryEvent e) {
