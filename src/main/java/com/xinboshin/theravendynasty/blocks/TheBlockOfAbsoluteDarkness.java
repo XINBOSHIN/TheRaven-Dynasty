f1afb63c878227d542ffc282be28d64502c6fd89
Xinboshin Butterworth <danilnovichek@vk.com>
2023-06-14 03:11:30 +0500

    Обновление документации к классу

--- src/main/java/com/xinboshin/theravendynasty/blocks/TheBlockOfAbsoluteDarkness.java
+++ src/main/java/com/xinboshin/theravendynasty/blocks/TheBlockOfAbsoluteDarkness.java
@@ -2,3 +2,6 @@ package com.xinboshin.theravendynasty.blocks;
 
 public class TheBlockOfAbsoluteDarkness {
 }
+
+
+//блок абсолютной тьмы, реализуйте гуишку или интерфейс прокачки (продумайте логику и добавьте крафт предмета в /item/s
